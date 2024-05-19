8abe71f1517fb397aaf58d1791c00886b6e46edd
Mayank <mayankpatil2605@gmail.com>
2024-05-19 16:36:23 -0500

    Changed the emojis

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,5 +1,5 @@
-import meh from '../assets/meh.webp';
-import bullsEye from '../assets/bulls-eye.webp';
+import coolFace from'../assets/cool-face.png'
+import goat from '../assets/Goat.jpg'
 import thumpsUp from '../assets/thumbs-up.webp';
 import { Image, ImageProps } from '@chakra-ui/react';
 
@@ -14,9 +14,9 @@ const Emoji = ({rating}: Props) => {
     const emojiMap : { [key: number] : ImageProps } = {
 
     
-        3: { src: meh ,alt : 'meh', boxSize:'25px'},
-        4: { src: thumpsUp ,alt : 'recommended', boxSize:'25px'},
-        5: { src: bullsEye , alt : 'exceptional', boxSize: '35px'}
+        3: { src: thumpsUp ,alt : 'meh', boxSize:'25px'},
+        4: { src: coolFace ,alt : 'recommended', boxSize:'25px'},
+        5: { src: goat , alt : 'exceptional', boxSize: '35px'}
     }
 
   return (
