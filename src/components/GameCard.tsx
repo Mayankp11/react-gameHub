f43da94a48491022e0efb89d09b57bec58a1e9bf
Mayank <mayankpatil2605@gmail.com>
2024-05-19 16:20:41 -0500

    Added Emojis for rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
 
 
 interface Props {
@@ -20,7 +21,7 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Heading fontSize={"2xl"}>{game.name} <Emoji rating={game.rating_top}/></Heading>
       </CardBody>
     </Card>
   );
